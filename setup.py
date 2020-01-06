7c8f7923e661d8d22ae7296ba49ea8d29ee90742
David Slusser <dslusser@box.com>
2020-01-05 16:04:37 -0800

    added registration templates; moved djangohelpers to handyhelpers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from setuptools import setup, find_packages
-import djangohelpers
+import handyhelpers
 
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as readme:
@@ -16,22 +16,21 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
-    version=djangohelpers.__version__,
-    license=djangohelpers.__license__,
-    author=djangohelpers.__author__,
-    author_email=djangohelpers.__email__,
+    version=handyhelpers.__version__,
+    license=handyhelpers.__license__,
+    author=handyhelpers.__author__,
+    author_email=handyhelpers.__email__,
     url='https://github.com/davidslusser/django-handyhelpers',
     download_url='https://github.com/davidslusser/django-handyhelpers/archive/{}.tar.gz'
-                 ''.format(djangohelpers.__version__),
+                 ''.format(handyhelpers.__version__),
     keywords=['django', 'helpers', 'handyhelpers'],
     install_requires=required,
     classifiers=[
         'Environment :: Web Environment',
-        'Framework :: Django :: 2.2+',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.x',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
