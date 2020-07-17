7a9eff6bc763975dea2d8e9e198a20be20cb1ddf
David Slusser <dslusser@box.com>
2020-07-16 21:21:31 -0700

    adding support for drf-dynamic-fields

--- handyhelpers/mixins/viewset_mixins.py
+++ handyhelpers/mixins/viewset_mixins.py
@@ -37,7 +37,7 @@ class InvalidLookupMixin:
 
     def dispatch(self, request, *args, **kwargs):
         for field, val in self.request.GET.dict().items():
-            if field in getattr(settings, 'INVALID_LOOKUP_SKIP_LIST',  ['format', 'fields']):
+            if field in getattr(settings, 'INVALID_LOOKUP_SKIP_LIST',  ['format', 'fields', 'omit', 'expand']):
                 continue
             if self.filter_class:
                 # if filter_class is available, return error if any query parameter is not a lookup expression
