bd2cc38969494467941e4ade13bd679aa31e7878
David Slusser <dslusser@box.com>
2020-11-16 20:41:47 -0800

    fixed template_name bug; added lookup

--- handyhelpers/lookups.py
+++ handyhelpers/lookups.py
@@ -1,5 +1,8 @@
 """
 This file contains custom filter lookups as per: https://docs.djangoproject.com/en/2.2/howto/custom-lookups/
+
+https://docs.djangoproject.com/en/2.2/ref/models/lookups/
+https://docs.djangoproject.com/en/2.2/ref/models/querysets/#field-lookups
 """
 
 from django.db.models import Lookup
@@ -13,3 +16,13 @@ class NotEqual(Lookup):
         rhs, rhs_params = self.process_rhs(compiler, connection)
         params = lhs_params + rhs_params
         return '%s <> %s' % (lhs, rhs), params
+
+
+class Exclude(Lookup):
+    lookup_name = 'excludes'
+
+    def as_sql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        params = lhs_params + rhs_params
+        return 'NOT (%s LIKE %s)' % (lhs, rhs), params
