8ccb4aa09eb43dd422a0f07eff65ca7adcb21d3c
David <36340871+davidslusser@users.noreply.github.com>
2021-01-01 16:09:28 -0800

    adding filterset_fields to InvalidLookupMixin (#39)

--- handyhelpers/mixins/viewset_mixins.py
+++ handyhelpers/mixins/viewset_mixins.py
@@ -8,7 +8,7 @@ class InvalidLookupMixin:
     """ A mixin for Django Rest Framework viewsets to check query parameters and return an error if any query parameter
     is not a included in defined in a filter_class (typically defined in your filterset), and element of
     filter_fields (typically set in your viewset), or a valid model field.
-    Order of precedence is: filter_class, filter_fields, model field.
+    Order of precedence is: filter_class, filterset_fields, filter_fields, model field.
 
     class parameters:
         request       - request object (as provided by Viewset)
@@ -24,6 +24,7 @@ class InvalidLookupMixin:
     model = None
     queryset = None
     filter_class = None
+    filterset_fields = []
     filter_fields = []
 
     def get_lookup_expression(self, fs_filter, related_field=None, lookup_expression_list=None):
@@ -64,6 +65,12 @@ class InvalidLookupMixin:
                     return JsonResponse(data={'detail': '{} is not a defined filter field in {}'
                                         .format(field, self.model.__name__)}, status=status.HTTP_404_NOT_FOUND)
 
+            elif self.filterset_fields:
+                # if filterset_fields are available, return error if any query parameter is not a filter field
+                if field not in self.filterset_fields:
+                    return JsonResponse(data={'detail': '{} is not a defined filter field in {}'
+                                        .format(field, self.model.__name__)}, status=status.HTTP_404_NOT_FOUND)
+
             elif self.filter_fields:
                 # if filter_fields are available, return error if any query parameter is not an available filter field
                 if field not in self.filter_fields:
@@ -71,7 +78,7 @@ class InvalidLookupMixin:
                                         .format(field, self.model.__name__)}, status=status.HTTP_404_NOT_FOUND)
 
             else:
-                # if neither filter_class nor filter_fields are available, return error if any query parameter is
+                # if neither filter_class nor filterset_fields are available, return error if any query parameter is
                 # not a field in the model
                 if field.split('__')[0] not in [i.name for i in self.model._meta.fields +
                                                                 self.model._meta.many_to_many]:
