f4db9e2c9c611a0610258bd09525d64bf9e615a2
David Slusser <david.slusser@myome.com>
2021-07-26 15:49:38 -0700

    updating drf permissions to be more in line with view permissions

--- handyhelpers/drf_permissions.py
+++ handyhelpers/drf_permissions.py
@@ -28,7 +28,6 @@ class IsInAllGroups(BasePermission):
                                'PUT': ['site_admins'],
                                'DELETE': ['site_admins', 'site_managers'],
                               }
-
     """
     def has_permission(self, request, view):
         required_groups_mapping = getattr(view, 'required_groups', {})
@@ -58,3 +57,72 @@ class IsInAnyGroup(BasePermission):
         if required_groups is None:
             return False
         return any(group in [i.name for i in request.user.groups.all()] for group in required_groups)
+
+
+class InAllGroups(BasePermission):
+    """
+    Description:
+        Restrict access based on request method and user group; user must be in ALL required groups
+
+    Usage:
+        put the following in your viewset:
+            permission_classes = (InAllGroups,)
+            permission_dict = {'POST': ['site_admins'],
+                               'PATCH': ['site_admins'],
+                               'PUT': ['site_admins'],
+                               'DELETE': ['site_admins', 'site_managers'],
+                              }
+    """
+
+    def has_permission(self, request, view):
+        if not hasattr(view, 'permission_dict'):
+            return False
+        if request.user.is_superuser:
+            return True
+        permission_dict_mapping = getattr(view, 'permission_dict', {})
+        permission_group_list = permission_dict_mapping.get(request.method, [])
+
+        # if method is not provided, deny operation
+        if permission_group_list is None:
+            return False
+
+        # if method is specified, but the group list is empty, allow operation
+        if permission_group_list == []:
+            return True
+
+        return set(permission_group_list).issubset([i.name for i in request.user.groups.all()])
+
+
+class InAnyGroup(BasePermission):
+    """
+    Description:
+        Restrict access based on request method and user group; user can be in ANY required group
+
+    Usage:
+        put the following in your viewset:
+            permission_classes = (InAnyGroup,)
+            permission_dict = {'POST': ['site_admins'],
+                               'PATCH': ['site_admins'],
+                               'PUT': ['site_admins'],
+                               'DELETE': ['site_admins', 'superusers'],
+                              }
+    """
+
+    def has_permission(self, request, view):
+        if not hasattr(view, 'permission_dict'):
+            return False
+        if request.user.is_superuser:
+            return True
+
+        permission_dict_mapping = getattr(view, 'permission_dict', {})
+        permission_group_list = permission_dict_mapping.get(request.method, None)
+
+        # if method is not provided, deny operation
+        if permission_group_list is None:
+            return False
+
+        # if method is specified, but the group list is empty, allow operation
+        if permission_group_list == []:
+            return True
+
+        return any(group in [i.name for i in request.user.groups.all()] for group in permission_group_list)
