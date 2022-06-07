0df527096d3cb469e922ca12eeae2f697d7b12d3
David <36340871+davidslusser@users.noreply.github.com>
2022-06-06 21:05:34 -0700

    moving user_tags to template tags (#74)

--- handyhelpers/mixins/form_mixins.py
+++ handyhelpers/mixins/form_mixins.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class SetRequiredMixin:
-    """ mixin class that sets fields as form fields as required or not_required based on the form Meta class parameters
+    """ mixin class that sets form fields as required or not_required based on the form Meta class parameters
      'required' and/or 'not_required'. Both parameters are a list of fields in the form. The 'required' parameter takes
      precedence over 'not_required' parameter if fields are listed in both lists.
 
