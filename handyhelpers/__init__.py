5e70ac6cecdf6268caf40a19ddddb83ec3bae410
David <36340871+davidslusser@users.noreply.github.com>
2022-06-16 21:10:05 -0700

    Update __init__.py

--- handyhelpers/__init__.py
+++ handyhelpers/__init__.py
@@ -7,7 +7,7 @@ A collection of handy utilities to support django operations.
 """
 
 __title__ = 'django-handyhelpers'
-__version__ = '0.1.20'
+__version__ = '0.1.21'
 __author__ = 'David Slusser'
 __email__ = 'dbslusser@gmail.com'
 __license__ = 'GPL-3.0'
