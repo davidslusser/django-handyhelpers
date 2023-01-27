56b534cd8d3e6f2f5963b5356087fc1710cbd7d7
davidslusser <dbslusser@gmail.com>
2023-01-26 17:49:08 -0800

    updates to viewset test generation

--- handyhelpers/management/commands/generate_unittests.py
+++ handyhelpers/management/commands/generate_unittests.py
@@ -1,6 +1,7 @@
-from django.core.management.base import BaseCommand, CommandError
 from django.apps import apps
 from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.urls import URLPattern, URLResolver
 from jinja2 import Template
 import os
 
@@ -32,15 +33,33 @@ class Command(BaseCommand):
 
         self.opts = options
         self.app = options['app']
-        self.get_model_list()
+        self.model_list = self.get_model_list()
+        files_created_list = []
 
         # build model unittests
         if 'model' in options['test_type']:
             self.build_model_unittests(output_path=options['output_path'],
                                        output_file=options['output_file'],
                                        template_file=options['template'])
+        elif 'viewset' in options['test_type']:
+            self.build_viewset_unittests(output_path=options['output_path'],
+                                         output_file=options['output_file'],
+                                         template_file=options['template'])
         else:
-            print('only model test are currently supported')
+            print(f'''unittest generation for {options['test_type']} is not currently supported''')
+
+        for item in files_created_list:
+            if item:
+                self.stdout.write(self.style.SUCCESS(f'generated {item}'))
+
+    @staticmethod
+    def my_import(name):
+        """ https://stackoverflow.com/questions/547829/how-to-dynamically-load-a-python-class """
+        components = name.split('.')
+        mod = __import__(components[0])
+        for comp in components[1:]:
+            mod = getattr(mod, comp)
+        return mod
 
     @staticmethod
     def get_model_field_names(model, exclude_list=()):
@@ -50,7 +69,9 @@ class Command(BaseCommand):
     def get_model_list(self):
         """ return a list of all models in application """
         app = apps.get_app_config(self.app)
-        self.model_list = sorted(list(app.get_models()), key=lambda x: x._meta.model_name)
+        model_list = list(app.get_models())
+        model_list.sort(key=lambda x: x.__name__)
+        return model_list
 
     def build_model_unittests(self, output_path=None, output_file=None, template_file=None):
         """ build unittest file for models """
@@ -98,3 +119,81 @@ class Command(BaseCommand):
         with open(output_path, 'w') as f:
             f.write(file_text)
         self.stdout.write(self.style.SUCCESS(f"{output_path} generated!"))
+
+    def build_viewset_unittests(self, output_path=None, output_file=None, template_file=None):
+        """ build unittest file for drf viewsets """
+        try:
+            if not template_file:
+                template_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                             'unittest_templates', 'viewsets.jinja')
+
+            if output_file:
+                output_path = output_file
+            elif not output_path:
+                output_path = 'test_viewsets.py'
+            elif os.path.isdir(output_path):
+                output_path = f'{output_path}/test_veiwsets.py'
+
+            test_dict = {} # {'view': view_object, url_list: list_of_route_names}
+            route_name_list = []
+            url_data = __import__(getattr(settings, 'ROOT_URLCONF'), {}, {}, [''])
+            app_name = self.app_name if self.app_name else self.app
+            app_urls = [i for i in url_data.urlpatterns if hasattr(i, 'app_name') and i.app_name == app_name]
+            for path in app_urls:
+                for item in path.url_patterns:
+                    if isinstance(item, URLResolver):
+                        for route in item.url_patterns:
+                            if route.name in ['api-root']:
+                                continue
+                            v = self.my_import(route.lookup_str)
+                            if not hasattr(v, 'model'):
+                                self.stdout.write(self.style.WARNING(f'INFO: can not find model in the viewset for '
+                                                                     f'{route.name}; skipping unittest generation'))
+                                continue
+                            if not hasattr(route, 'app_name'):
+                                route.app_name = path.app_name
+                            if v in test_dict:
+                                if route not in test_dict[v]:
+                                    if route.name not in route_name_list:
+                                        test_dict[v].append(route)
+                                        route_name_list.append(route.name)
+                            else:
+                                if route.name not in route_name_list:
+                                    test_dict[v] = [route]
+                                    route_name_list.append(route.name)
+
+            data = dict(data=test_dict)
+            with open(template_file) as f:
+                template = Template(f.read())
+            file_text = template.render(data)
+            with open(output_path, 'w') as f:
+                f.write(file_text)
+            return output_path
+
+        except Exception as err:
+            self.stdout.write(self.style.ERROR(f'error generating viewset unittests: {err}'))
+
+
+    def build_view_unittests(self, output_path=None, output_file=None, template_file=None):
+        """ build unittest file for views """
+        if not template_file:
+            template_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                         'unittest_templates', 'viewsets.jinja')
+
+        if output_file:
+            output_path = output_file
+        elif not output_path:
+            output_path = 'test_viewsets.py'
+        elif os.path.isdir(output_path):
+            output_path = f'{output_path}/test_veiwsets.py'
+
+        app_url_list = []
+        url_data = __import__(getattr(settings, 'ROOT_URLCONF'), {}, {}, [''])
+        app_urls = [i for i in url_data.urlpatterns if hasattr(i, 'app_name') and i.app_name == self.app]
+        for path in app_urls:
+            for item in path.url_patterns:
+                if isinstance(item, URLPattern):
+                    if item in app_url_list:
+                        continue
+                    app_url_list.append(item)
+                    print(item.name, item.lookup_str, item.pattern)
