52883a93a9d400e993235288bb798e5ddd28329b
davidslusser <dbslusser@gmail.com>
2023-04-12 17:30:30 -0700

    updating templates and styles; using cbv for ajax

--- handyhelpers/views/ajax.py
+++ handyhelpers/views/ajax.py
@@ -1,118 +1,31 @@
 import json
 import psutil
 
+from django.views import View
 from django.apps import apps
 from django.http import HttpResponse
 from django.template import loader
-from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.http import require_GET
 
 from auditlog.models import LogEntry
 
 
-@require_GET
-def get_host_network_stats(request):
-    """
-    Description:
-        Get network statistics for a given network interface on the host machine.
-    Args:
-        request: AJAX request object.
-    Returns:
-        HttpResponse: JSON formatted response.
-    """
-    if (request.is_ajax()) and (request.method == 'GET'):
-        if 'client_response' in request.GET:
-            interface = request.GET['client_response']
-            data = psutil.net_if_stats()[interface]
-            template = loader.get_template('handyhelpers/ajax/host_interface_stats.htm')
-            return HttpResponse(json.dumps({'server_response': template.render({'data': data})}),
-                                content_type='application/javascript')
-        else:
+class AjaxGetView(View):
+    """ generic view for handling GET requests sent via ajax """
+    template = None
+    data = {}
+
+    def get(self, request, *args, **kwargs):
+        try:
+            if not self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return HttpResponse('Invalid request', status=400)    
+            if 'client_response' in self.request.GET:
+                return HttpResponse(json.dumps({'server_response': self.template.render({'data': self.data})}),
+                                    content_type='application/javascript')
             return HttpResponse('Invalid request inputs', status=400)
-    else:
-        return HttpResponse('Invalid request', status=400)
-
-
-@require_GET
-def get_host_process_details(request):
-    """
-    Description:
-        Get process details for a given process on the host machine.
-    Args:
-        request: AJAX request object.
-    Returns:
-        HttpResponse: JSON formatted response.
-    """
-    if (request.is_ajax()) and (request.method == 'GET'):
-        if 'client_response' in request.GET:
-            proc = request.GET['client_response']
-            try:
-                data = psutil.Process(int(proc))
-            except psutil.AccessDenied:
-                data = None
-            template = loader.get_template('handyhelpers/ajax/host_process_details.htm')
-            return HttpResponse(json.dumps({'server_response': template.render({'data': data.as_dict()})}),
-                                content_type='application/javascript')
-        else:
-            return HttpResponse('Invalid request inputs', status=400)
-    else:
-        return HttpResponse('Invalid request', status=400)
-
-
-@require_GET
-def get_host_partition_usage(request):
-    """
-    Description:
-        Get disk usage for a given partition on the host machine.
-    Args:
-        request: AJAX request object.
-    Returns:
-        HttpResponse: JSON formatted response.
-    """
-    if (request.is_ajax()) and (request.method == 'GET'):
-        if 'client_response' in request.GET:
-            part = request.GET['client_response']
-            try:
-                data = psutil.disk_usage(part)
-            except psutil.AccessDenied:
-                data = None
-            template = loader.get_template('handyhelpers/ajax/host_partition_usage.htm')
-            return HttpResponse(json.dumps({'server_response': template.render({'data': data})}),
-                                content_type='application/javascript')
-        else:
-            return HttpResponse('Invalid request inputs', status=400)
-    else:
-        return HttpResponse('Invalid request', status=400)
-
-
-@require_GET
-def get_host_cpu_stats(request):
-    """
-    Description:
-        Get CPU status for a given cpu on the host machine.
-    Args:
-        request: AJAX request object.
-    Returns:
-        HttpResponse: JSON formatted response.
-    """
-    if (request.is_ajax()) and (request.method == 'GET'):
-        if 'client_response' in request.GET:
-            cpu = int(request.GET['client_response'])
-            try:
-                data = dict(
-                    time=psutil.cpu_times(percpu=True)[cpu],
-                    time_percent=psutil.cpu_times_percent(percpu=True)[cpu],
-                    frequency=psutil.cpu_freq(percpu=True)[cpu],
-                )
-
-            except psutil.AccessDenied:
-                data = None
-            template = loader.get_template('handyhelpers/ajax/host_cpu_stats.htm')
-            return HttpResponse(json.dumps({'server_response': template.render({'data': data})}),
-                                content_type='application/javascript')
-        else:
-            return HttpResponse('Invalid request inputs', status=400)
-    else:
-        return HttpResponse('Invalid request', status=400)
+        except Exception:
+            return HttpResponse(json.dumps({'server_response': self.template.render({'data': None})}),
+                                    content_type='application/javascript')
 
 
 @require_GET
@@ -136,7 +49,6 @@ def get_auditlog(request, *args, **kwargs):
             return HttpResponse('Invalid request inputs', status=400)
     else:
         return HttpResponse('Invalid request', status=400)
-
 
 
 @require_GET
@@ -186,3 +98,79 @@ def get_auditlog_entry_details(request):
             return HttpResponse('Invalid request inputs', status=400)
     else:
         return HttpResponse('Invalid request', status=400)
+
+
+       
+class GetHostNetworkStats(AjaxGetView):
+    """
+    Description:
+        Get network statistics for a given network interface on the host machine.
+    Args:
+        request: AJAX request object.
+    Returns:
+        HttpResponse: JSON formatted response.
+    """
+    template = loader.get_template('handyhelpers/ajax/host_interface_stats.htm')
+    
+    def get(self, request, *args, **kwargs):
+        interface = self.request.GET['client_response']
+        self.data = psutil.net_if_stats()[interface]
+        return super().get(request, *args, **kwargs)
+
+
+class GetHostProcessDetails(AjaxGetView):
+    """
+    Description:
+        Get process details for a given process on the host machine.
+    Args:
+        request: AJAX request object.
+    Returns:
+        HttpResponse: JSON formatted response.
+    """
+    template = loader.get_template('handyhelpers/ajax/host_process_details.htm')
+    
+    def get(self, request, *args, **kwargs):
+        proc = request.GET['client_response']
+        try:
+            self.data = psutil.Process(int(proc))
+        except psutil.AccessDenied:
+            self.data = {}
+        return super().get(request, *args, **kwargs)
+    
+    
+class GetHostParitionUsage(AjaxGetView):
+    """
+    Description:
+        Get disk usage for a given partition on the host machine.
+    Args:
+        request: AJAX request object.
+    Returns:
+        HttpResponse: JSON formatted response.
+    """
+    template = loader.get_template('handyhelpers/ajax/host_partition_usage.htm')
+    
+    def get(self, request, *args, **kwargs):
+        part = request.GET['client_response']
+        self.data = psutil.disk_usage(part)
+        return super().get(request, *args, **kwargs)
+
+
+class GetHostCpuStats(AjaxGetView):
+    """
+    Description:
+        Get CPU status for a given cpu on the host machine.
+    Args:
+        request: AJAX request object.
+    Returns:
+        HttpResponse: JSON formatted response.
+    """
+    template = loader.get_template('handyhelpers/ajax/host_cpu_stats.htm')
+    
+    def get(self, request, *args, **kwargs):
+        cpu = int(request.GET['client_response'])
+        self.data = dict(
+            time=psutil.cpu_times(percpu=True)[cpu],
+            time_percent=psutil.cpu_times_percent(percpu=True)[cpu],
+            frequency=psutil.cpu_freq(percpu=True)[cpu],
+        )
+        return super().get(request, *args, **kwargs)
