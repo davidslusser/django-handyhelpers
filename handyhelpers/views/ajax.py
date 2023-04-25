3054f3bba05a5f29cb691d7fe1ec0aa3e1b6c635
davidslusser <dbslusser@gmail.com>
2023-04-25 10:18:58 -0700

    consolidating host views to single file

--- handyhelpers/views/ajax.py
+++ handyhelpers/views/ajax.py
@@ -1,9 +1,7 @@
 import json
-import psutil
 
 from django.views import View
 from django.http import HttpResponse
-from django.template import loader
 
 
 class AjaxGetView(View):
@@ -22,78 +20,3 @@ class AjaxGetView(View):
         except Exception:
             return HttpResponse(json.dumps({'server_response': self.template.render({'data': None})}),
                                     content_type='application/javascript')
-
-       
-class GetHostNetworkStats(AjaxGetView):
-    """
-    Description:
-        Get network statistics for a given network interface on the host machine.
-    Args:
-        request: AJAX request object.
-    Returns:
-        HttpResponse: JSON formatted response.
-    """
-    template = loader.get_template('handyhelpers/ajax/host_interface_stats.htm')
-    
-    def get(self, request, *args, **kwargs):
-        interface = self.request.GET['client_response']
-        self.data = psutil.net_if_stats()[interface]
-        return super().get(request, *args, **kwargs)
-
-
-class GetHostProcessDetails(AjaxGetView):
-    """
-    Description:
-        Get process details for a given process on the host machine.
-    Args:
-        request: AJAX request object.
-    Returns:
-        HttpResponse: JSON formatted response.
-    """
-    template = loader.get_template('handyhelpers/ajax/host_process_details.htm')
-    
-    def get(self, request, *args, **kwargs):
-        proc = request.GET['client_response']
-        try:
-            self.data = psutil.Process(int(proc))
-        except psutil.AccessDenied:
-            self.data = {}
-        return super().get(request, *args, **kwargs)
-    
-    
-class GetHostParitionUsage(AjaxGetView):
-    """
-    Description:
-        Get disk usage for a given partition on the host machine.
-    Args:
-        request: AJAX request object.
-    Returns:
-        HttpResponse: JSON formatted response.
-    """
-    template = loader.get_template('handyhelpers/ajax/host_partition_usage.htm')
-    
-    def get(self, request, *args, **kwargs):
-        part = request.GET['client_response']
-        self.data = psutil.disk_usage(part)
-        return super().get(request, *args, **kwargs)
-
-
-class GetHostCpuStats(AjaxGetView):
-    """
-    Description:
-        Get CPU status for a given cpu on the host machine.
-    Args:
-        request: AJAX request object.
-    Returns:
-        HttpResponse: JSON formatted response.
-    """
-    template = loader.get_template('handyhelpers/ajax/host_cpu_stats.htm')
-    
-    def get(self, request, *args, **kwargs):
-        cpu = int(request.GET['client_response'])
-        self.data = dict(
-            time=psutil.cpu_times(percpu=True)[cpu],
-            time_percent=psutil.cpu_times_percent(percpu=True)[cpu],
-            frequency=psutil.cpu_freq(percpu=True)[cpu],
-        )
-        return super().get(request, *args, **kwargs)
--- handyhelpers/views/host.py
+++ handyhelpers/views/host.py
@@ -3,7 +3,9 @@ import psutil
 
 from dateutil.relativedelta import relativedelta
 from django.shortcuts import render
+from django.template import loader
 from django.views.generic import (View)
+from handyhelpers.views.ajax import AjaxGetView
 
 # import forms
 from handyhelpers.forms import HostProcessFilterForm
@@ -142,3 +144,78 @@ class ShowHostCpu(View):
         context['load_avg_1'], context['load_avg_5'], context['load_avg_15'] = \
             [round(x / psutil.cpu_count() * 100, 2) for x in psutil.getloadavg()]
         return render(request, self.template_name, context=context)
+
+
+class GetHostNetworkStats(AjaxGetView):
+    """
+    Description:
+        Get network statistics for a given network interface on the host machine.
+    Args:
+        request: AJAX request object.
+    Returns:
+        HttpResponse: JSON formatted response.
+    """
+    template = loader.get_template('handyhelpers/ajax/host_interface_stats.htm')
+    
+    def get(self, request, *args, **kwargs):
+        interface = self.request.GET['client_response']
+        self.data = psutil.net_if_stats()[interface]
+        return super().get(request, *args, **kwargs)
+
+
+class GetHostProcessDetails(AjaxGetView):
+    """
+    Description:
+        Get process details for a given process on the host machine.
+    Args:
+        request: AJAX request object.
+    Returns:
+        HttpResponse: JSON formatted response.
+    """
+    template = loader.get_template('handyhelpers/ajax/host_process_details.htm')
+    
+    def get(self, request, *args, **kwargs):
+        proc = request.GET['client_response']
+        try:
+            self.data = psutil.Process(int(proc))
+        except psutil.AccessDenied:
+            self.data = {}
+        return super().get(request, *args, **kwargs)
+    
+    
+class GetHostParitionUsage(AjaxGetView):
+    """
+    Description:
+        Get disk usage for a given partition on the host machine.
+    Args:
+        request: AJAX request object.
+    Returns:
+        HttpResponse: JSON formatted response.
+    """
+    template = loader.get_template('handyhelpers/ajax/host_partition_usage.htm')
+    
+    def get(self, request, *args, **kwargs):
+        part = request.GET['client_response']
+        self.data = psutil.disk_usage(part)
+        return super().get(request, *args, **kwargs)
+
+
+class GetHostCpuStats(AjaxGetView):
+    """
+    Description:
+        Get CPU status for a given cpu on the host machine.
+    Args:
+        request: AJAX request object.
+    Returns:
+        HttpResponse: JSON formatted response.
+    """
+    template = loader.get_template('handyhelpers/ajax/host_cpu_stats.htm')
+    
+    def get(self, request, *args, **kwargs):
+        cpu = int(request.GET['client_response'])
+        self.data = dict(
+            time=psutil.cpu_times(percpu=True)[cpu],
+            time_percent=psutil.cpu_times_percent(percpu=True)[cpu],
+            frequency=psutil.cpu_freq(percpu=True)[cpu],
+        )
+        return super().get(request, *args, **kwargs)
