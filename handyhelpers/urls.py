fc6928e773405406f7c35be4fdc865225b6605b0
davidslusser <dbslusser@gmail.com>
2023-06-12 16:06:14 -0700

    updating bootstrap; removing host views/templates; now available in django-hostutils

--- handyhelpers/urls.py
+++ handyhelpers/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.urls import path
 from handyhelpers.views import action
-from handyhelpers.views import host
-from handyhelpers.views import htmx
 
 if 'auditlog' in settings.INSTALLED_APPS:
     from handyhelpers.views import auditlog
@@ -11,27 +9,9 @@ if 'auditlog' in settings.INSTALLED_APPS:
 app_name = 'handyhelpers'
 
 urlpatterns = [
-    # host views
-    path('host_details/', host.ShowHost.as_view(), name='host_details'),
-    path('host_network/', host.ShowHostNetwork.as_view(), name='host_network'),
-    path('host_process/', host.ShowHostProcesses.as_view(), name='host_process'),
-    path('host_disk/', host.ShowHostDisk.as_view(), name='host_disk'),
-    path('host_memory/', host.ShowHostMemory.as_view(), name='host_memory'),
-    path('host_cpu/', host.ShowHostCpu.as_view(), name='host_cpu'),
-
     # action views
     path('filter_list_view', action.FilterListView.as_view(), name='filter_list_view'),
     path('show_all_list_view', action.ShowAllListView.as_view(), name='show_all_list_view'),
-
-    # ajax views
-    path('host_interface_stats/', host.GetHostNetworkStats.as_view(), name='host_interface_stats'),
-    path('host_process_details/', host.GetHostProcessDetails.as_view(), name='host_process_details'),
-    path('host_partition_usage/', host.GetHostParitionUsage.as_view(), name='host_partition_usage'),
-    path('get_host_cpu_stats/', host.GetHostCpuStats.as_view(), name='get_host_cpu_stats'),
-
-    # htmx views
-    path('get_host_processes/', htmx.GetHostProcesses.as_view(), name='get_host_processes'),
-
 ]
 
 if 'auditlog' in settings.INSTALLED_APPS:
--- handyhelpers/views/htmx.py
+++ handyhelpers/views/htmx.py
@@ -1,83 +1,8 @@
-import datetime
-import psutil
 
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from django.views.generic import View
-
-
-# import forms
-from handyhelpers.forms import HostProcessFilterForm
-
-
-class GetHostProcesses(View):
-    def get(self, request):
-        context = dict()
-        process_list = list(psutil.process_iter())
-        filter_form = HostProcessFilterForm(request.GET or None)
-
-        counts = dict(
-            running=len([i for i in process_list if i.status() == 'running']),
-            sleeping=len([i for i in process_list if i.status() == 'sleeping']),
-            idle=len([i for i in process_list if i.status() == 'idle']),
-            stopped=len([i for i in process_list if i.status() == 'stopped']),
-            zombie=len([i for i in process_list if i.status() == 'zombie']),
-            dead=len([i for i in process_list if i.status() == 'dead']),
-        )
-        context['counts'] = counts
-
-        # check for form clearing
-        if request.GET.dict().get('clear', None):
-            context['clear_filter'] = False
-
-        else:
-            if filter_form.is_valid():
-                context['clear_filter'] = True
-                if filter_form.cleaned_data.get('status', None):
-                    filtered_process_list = list()
-                    for i in process_list:
-                        try:
-                            if i.status() in filter_form.cleaned_data['status']:
-                                filtered_process_list.append(i)
-                        except psutil.NoSuchProcess:
-                            continue
-                    process_list = filtered_process_list
-
-                if filter_form.cleaned_data.get('created_at__gte', None):
-                    filtered_process_list = list()
-                    for i in process_list:
-                        try:
-                            if i.create_time() > filter_form.cleaned_data['created_at__gte'].timestamp():
-                                filtered_process_list.append(i)
-                        except psutil.NoSuchProcess:
-                            continue
-                    process_list = filtered_process_list
-                if filter_form.cleaned_data.get('created_at__lte', None):
-                    filtered_process_list = list()
-                    for i in process_list:
-                        try:
-                            if i.create_time() < filter_form.cleaned_data['created_at__lte'].timestamp():
-                                filtered_process_list.append(i)
-                        except psutil.NoSuchProcess:
-                            continue
-                    process_list = filtered_process_list
-        context['process_list'] = process_list
-        context['title'] = 'Host Processes'
-        context['now'] = datetime.datetime.now()
-        context['subtitle'] = psutil.os.uname()[1]
-        filter_form = dict()
-        filter_form['form'] = HostProcessFilterForm(request.GET or None)
-        filter_form['modal_name'] = 'filter_processes'
-        filter_form['modal_size'] = 'modal-lg'
-        filter_form['modal_title'] = 'Filter Host Processes'
-        filter_form['hx_method'] = 'hx-get'
-        filter_form['hx_url'] = '/handyhelpers/get_host_processes'
-        filter_form['hx_target'] = 'id_process_list_container'
-        filter_form['method'] = 'GET'
-        filter_form['action'] = 'Filter'
-        context['filter_form'] = filter_form
-        return render(request, template_name='handyhelpers/snippets/host_process_card_swap.htm', context=context)
 
 
 class GenericHtmxView(View):
