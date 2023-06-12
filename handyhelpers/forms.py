3e258b75379a0269036ff8cd5798c9004b36e809
David <36340871+davidslusser@users.noreply.github.com>
2023-06-12 16:08:29 -0700

    updating bootstrap; removing host views/templates; now available in django-hostutils (#106)

--- handyhelpers/forms.py
+++ handyhelpers/forms.py
@@ -35,18 +35,3 @@ class TimestampFilter(forms.Form):
                                       required=False, label='Updated After')
     updated_at__lte = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
                                       required=False, label='Updated Before')
-
-
-class HostProcessFilterForm(forms.Form):
-    """ Form Class for filtering host processes """
-    status_choices = [(i, i) for i in ['running', 'idle', 'sleeping', 'stopped', 'zombie', 'dead']]
-    status = forms.MultipleChoiceField(choices=status_choices,
-                                       widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
-                                       required=False,
-                                       label='Status')
-    created_at__gte = forms.DateTimeField(widget=forms.DateInput(attrs={'class': 'form-control',
-                                                                        'type': 'datetime-local'}),
-                                          required=False, label='Created After')
-    created_at__lte = forms.DateTimeField(widget=forms.DateInput(attrs={'class': 'form-control',
-                                                                        'type': 'datetime-local'}),
-                                          required=False, label='Created Before')
