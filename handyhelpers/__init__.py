27a6d9805dd5995fafad6b0287607286c8b6cde5
davidslusser <dbslusser@gmail.com>
2024-02-12 20:05:56 -0800

    move table component js and css to separate blocks

--- handyhelpers/__init__.py
+++ handyhelpers/__init__.py
@@ -7,7 +7,7 @@ A collection of handy utilities to support django operations.
 """
 
 __title__ = 'django-handyhelpers'
-__version__ = '0.3.11'
+__version__ = '0.3.12'
 __author__ = 'David Slusser'
 __email__ = 'dbslusser@gmail.com'
 __license__ = 'GPL-3.0'
