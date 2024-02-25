d65f369cea861d48d99a8a75b5f9b74b98a097f3
David <36340871+davidslusser@users.noreply.github.com>
2024-02-24 17:16:52 -0800

    Htmx views (#118)
    * adding sidebar view and htmx mixin
    
    * adding ids to base template div; adding htmx view for building sidebar

--- handyhelpers/mixins/view_mixins.py
+++ handyhelpers/mixins/view_mixins.py
@@ -144,3 +144,16 @@ class TimestampFilterMixin:
         # add filter form
         if self.filter_form:
             self.context['filter_form'] = self.filter_form(self.request.POST or None, initial=query_params)
+
+
+class HtmxViewMixin:
+    """ Mixin used to check if a request is htmx."""
+    def is_htmx(self):
+        """Check if a request is htmx; determined by inclusion of Hx-Request header.
+
+        Returns:
+            True if request is htmx, otherwise False
+        """
+        if self.request.headers.get("Hx-Request", None):
+            return True
+        return False
--- handyhelpers/views/htmx.py
+++ handyhelpers/views/htmx.py
@@ -4,31 +4,11 @@ from django.template import loader
 from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic import View
+from handyhelpers.mixins.view_mixins import HtmxViewMixin
 
 
-class GenericHtmxView(View):
-    """Generic view for handling HTMX requests; intended to be used with snippets/partials.
-
-    context       - context dictionary containing data to be rendered in template
-    template_name - template used when rendering page; defaults to: handyhelpers/generic/generic_list.html
-    """
-
-    template_name = None
-    queryset = None
-
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-
-    def get(self, request, context={}, *args, **kwargs):
-        if not self.request.headers.get("Hx-Request", None):
-            return HttpResponse("Invalid request", status=400)
-        if self.queryset:
-            context["queryset"] = self.queryset
-        return render(request, self.template_name, context)
-
-
-class BuildBootstrapModalView(GenericHtmxView):
-    """Generic view used to build a Boostrap 5 modal via HTMX.
+class BuildBootstrapModalView(HtmxViewMixin, View):
+    """Generic view used to build a Boostrap 5 modal via htmx.
 
     class parameters:
         template_name       - template used to render modal; defaults to "handyhelpers/htmx/bs5/modal_swap.htm"
@@ -50,8 +30,8 @@ class BuildBootstrapModalView(GenericHtmxView):
     data = {}
     extra_data = {}
 
-    def get(self, request, context={}, *args, **kwargs):
-        if not self.request.headers.get("Hx-Request", None):
+    def get(self, request):
+        if not self.is_htmx():
             return HttpResponse("Invalid request", status=400)
         context = {
             "modal_title": self.modal_title,
@@ -63,7 +43,7 @@ class BuildBootstrapModalView(GenericHtmxView):
             "data": self.data,
             "extra_data": self.extra_data,
         }
-        return super().get(request, context, *args, **kwargs)
+        return render(request, self.template_name, context)
 
 
 class AboutProjectModalView(BuildBootstrapModalView):
@@ -94,3 +74,77 @@ class AboutProjectModalView(BuildBootstrapModalView):
                 "handyhelpers/htmx/bs5/about_project_modal_body.htm", context=context
             )
         return super().get(request, *args, **kwargs)
+
+
+class HtmxSidebarItems(HtmxViewMixin, View):
+    """View to process a htmx request and render a partial template intended to be used on a sidebar navigation.
+
+    template_name - template used when rendering partial; defaults to: handyhelpers/htmx/bs5/sidebar_items.htm
+    queryset      - queryset containing data to be rendered on the partial
+
+    Usage Example:
+        class GetSomeData(HtmxSidebarItems):
+            queryset = MyModel.objects.all()
+    """
+
+    template_name = "handyhelpers/htmx/bs5/sidebar_menu_sub_items.htm"
+    queryset = None
+
+    def get(self, request, *args, **kwargs):
+        if not self.is_htmx():
+            return HttpResponse("Invalid request", status=400)
+        if self.queryset is not None:
+            self.queryset._result_cache = None
+        self.context = dict(
+            queryset=self.queryset,
+            args=self.args,
+            kwargs=self.kwargs,
+        )
+        return render(request, self.template_name, self.context)
+
+
+
+class BuildModelSidebarNav(HtmxViewMixin, View):
+    """Dynamically build a sidebar navigation menu where items included are sourced from application models.
+    This is intended handyhelpers_with_sidebar.htm or similar template.
+
+    Class Parameters:
+        template_name  - template used when rendering page; defaults to:
+                         handyhelpers/htmx/bs5/navigation/build_sidebar.htm
+        menu_item_list - list of dictionaries containing querysets (required) and icons (optional) to use in side menu
+
+    Usage Example:
+
+        class BuildSidebar(BuildSidebarNav):
+            menu_item_list = [
+                {"queryset": MyModelOne.objects.filter(date_time__gte=timezone.now()),
+                "icon": '<i class="fa-solid fa-calendar-day"></i>',
+                },
+                {"queryset": MyModelOne.objects.filter(enabled=True),
+                "icon": '<i class="fa-solid fa-people-group"></i>',
+                "htmx_link": False,
+                },
+            ]
+    """
+
+    template_name = "handyhelpers/htmx/bs5/navigation/build_sidebar.htm"
+    menu_item_list = []
+
+    def get(self, request):
+        if not self.is_htmx():
+            return HttpResponse("Invalid request", status=400)
+        for item in self.menu_item_list:
+            queryset = item["queryset"]
+            if queryset is not None:
+                queryset._result_cache = None
+            item["model_name"] = queryset.model._meta.verbose_name_plural
+            item["target_id"] = queryset.model._meta.verbose_name_plural.replace(
+                " ", "_"
+            )
+            item["link"] = hasattr(queryset.model, "get_absolute_url")
+            if item.get("htmx_link", True) and not item.get("htmx_target", None):
+                item["htmx_target"] = "body_main"
+        self.context = dict(
+            menu_item_list=self.menu_item_list,
+        )
+        return render(request, self.template_name, self.context)
