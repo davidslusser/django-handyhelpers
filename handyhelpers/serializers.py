2f18bf4c944a52b3d92f5fa7368052cb44e67ce0
David <36340871+davidslusser@users.noreply.github.com>
2025-01-16 12:56:45 -0800

    adding serializer mixin (#141)

--- handyhelpers/serializers.py
+++ handyhelpers/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
+from mixins.serializer_mixins import FlexFieldsMixin
 
 
 class FkReadWriteField(serializers.RelatedField):
-    """ A generic serializer field that provides the string representation of a ForeignKey object, as provided
+    """A generic serializer field that provides the string representation of a ForeignKey object, as provided
     by the __str__ model method, for a read operation, and performs a lookup using the provided lookup_field for
     write operations.
 
     Example usage:
         my_fk_field = FkReadWriteField(required=False, queryset=MyModel.objects.all(), lookup_field='fk_field_name')
     """
+
     def __init__(self, **kwargs):
         self.model = kwargs["queryset"].model
         self.lookup_field = kwargs.pop("lookup_field", None)
@@ -24,6 +26,24 @@ class FkReadWriteField(serializers.RelatedField):
         try:
             return self.model.objects.get(**{self.lookup_field: data})
         except self.model.DoesNotExist:
-            raise serializers.ValidationError(f"{self.model._meta.model_name} with {self.lookup_field} '{data}' not found")
+            raise serializers.ValidationError(
+                f"{self.model._meta.model_name} with {self.lookup_field} '{data}' not found"
+            )
         except self.model.MultipleObjectsReturned:
-            raise serializers.ValidationError(f"multiple objects returned; lookup_field must be unique")
+            raise serializers.ValidationError(
+                f"multiple objects returned; lookup_field must be unique"
+            )
+
+
+class NoModelFlexSerializer(FlexFieldsMixin, serializers.Serializer):
+    """A serializer that provides dynamical filtering of fields in the return data.
+
+    Example usage:
+        class ExampleViewSet(viewsets.ViewSet):
+            def list(self, request):
+                data = {}
+                serializer = NoModelSerializer(data, context={"request": request})
+                return Response(serializer.data)
+    """
+
+    pass
