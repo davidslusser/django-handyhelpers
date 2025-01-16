b888728575ac29971fd2e708a7109e8ce78c482c
David Slusser <dbslusser@gmail.com>
2025-01-16 12:55:34 -0800

    adding serializer mixin

---
+++ handyhelpers/mixins/serializer_mixins.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+
+
+class FlexFieldsMixin:
+    """A serializer mixin that allow filtering or omitting fields by query parameters"""
+
+    query_params: dict = {}
+
+    def __init__(self, *args, **kwargs) -> None:
+        request = kwargs.get("context", {}).get("request", None)
+
+        if request:
+            self.query_params = request.query_params
+        super().__init__(*args, **kwargs)
+
+    def get_fields(self):
+        fields = super().get_fields()
+        if self.query_params:
+            fields_param: str = getattr(settings, "HH_FIELDS_PARAM", "fields")
+            omit_param: str = getattr(settings, "HH_OMIT_PARAM", "omit")
+            requested_fields = self.query_params.get(fields_param, None)
+            omitted_fields = self.query_params.get(omit_param, None)
+        if requested_fields:
+            return {
+                key: value
+                for key, value in fields.items()
+                if key in requested_fields.split(",")
+            }
+        elif omitted_fields:
+            return {
+                key: value
+                for key, value in fields.items()
+                if key not in requested_fields.split(",")
+            }
+        return fields
